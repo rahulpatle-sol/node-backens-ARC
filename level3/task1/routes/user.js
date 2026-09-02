25c16fcbe7ed229106decc542bf85231d34ade9a
rahulpatle-sol <patler044@gmail.com>
2026-09-02 16:40:36 +0530

    task  2 crud app done

--- level3/task1/routes/user.js
+++ level3/task1/routes/user.js
@@ -18,4 +18,68 @@ router.post('/users', async (req, res) => {
   }
 });
 
+
+
+//  task  2 is here 
+//  get users also
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  }
+    catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//  get by  id 
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  }     
+catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+//  put 
+
+router.put('/users/:id', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email, password },
+      { new: true }
+    );
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//  delete user
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+
 export default router;
+//  CRUD APP  IS HERE 

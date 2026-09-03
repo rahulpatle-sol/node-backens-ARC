3b009fb2f6a1f1585f8df6d528b55c3841fa6096
rahulpatle-sol <patler044@gmail.com>
2026-09-03 18:38:23 +0530

    feat : added ratelimiter

--- level5/task1/app.js
+++ level5/task1/app.js
@@ -3,13 +3,17 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import usersRouter from './routes/Users.js';
 import errorHandler from '../../level4/task1/auth/middleware/errorHandler.js';
+import rateLimiter from './middlewares/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(rateLimiter);
 app.use('/api', usersRouter);
+
+
 app.use(errorHandler);
 
 export default app;
